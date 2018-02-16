ec2d68f183c1229b50ad4dc139a1df4fbb7785a5
Unknown <vanessa.osterberg@outlook.com>
2018-02-15 19:36:39 -0600

    Added Array

--- src/TestGreet.java
+++ src/TestGreet.java
@@ -3,8 +3,16 @@ public class TestGreet {
 
 	public static void main(String[] args) {
 
-		Greet billy = new Greet();
-		billy.toString();
+		Greet[] bob = new Greet[40];
+		for(int counter = 0; counter < bob.length; counter ++)
+		{
+			Greet billy = new Greet();
+			bob[counter] = billy;
+		}
+		for(Greet temp: bob)
+		{
+			temp.toString();
+		}
 
 	}
 
