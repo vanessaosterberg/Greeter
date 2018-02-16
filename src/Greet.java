0f16465cbca027e2112aad65650102d8d3241bc8
Unknown <vanessa.osterberg@outlook.com>
2018-02-15 19:32:06 -0600

    Greet Class

---
+++ src/Greet.java
@@ -0,0 +1,22 @@
+
+public class Greet {
+	private String name;
+	private int age;
+	
+	public Greet() {
+		name = billy;
+		age = 10;
+	}
+	
+	public Greet(String xName, int xAge){
+		name = xName;
+		age = xAge;
+	}
+	
+	
+		public String toString(){
+			return "Greet: name = " + name + " age = " + age;
+		}
+	
+
+}
